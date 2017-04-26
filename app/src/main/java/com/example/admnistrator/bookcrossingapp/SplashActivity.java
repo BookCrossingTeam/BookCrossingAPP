e57c7811add179a7e167b35d7073be3e9d079859
OUC-MYM <570109323@qq.com>
2017-04-26 21:54:20 +0800

    APP启动页实现
    SplashActivity启动

---
+++ app/src/main/java/com/example/admnistrator/bookcrossingapp/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.admnistrator.bookcrossingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        toNextPage();
+    }
+
+    public void toNextPage()
+    {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+
+
+}
