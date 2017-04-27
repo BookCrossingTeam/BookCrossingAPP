84b487235a35a90086dc876e13e52043e61b8172
OUC-MYM <570109323@qq.com>
2017-04-27 09:40:33 +0800

    修复bug

--- app/src/main/java/com/example/admnistrator/bookcrossingapp/SplashActivity.java
+++ app/src/main/java/com/example/admnistrator/bookcrossingapp/SplashActivity.java
@@ -1,12 +1,9 @@
 package com.example.admnistrator.bookcrossingapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-
-import static android.os.Build.VERSION_CODES.M;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -23,7 +20,7 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-               tent intent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
