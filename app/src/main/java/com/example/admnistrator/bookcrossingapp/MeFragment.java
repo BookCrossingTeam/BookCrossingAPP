341f08203f3abd2746b9697e8ca18d456bad7822
lin2270707 <541510068@qq.com>
2018-04-03 20:42:25 +0800

    a

--- app/src/main/java/com/example/admnistrator/bookcrossingapp/MeFragment.java
+++ app/src/main/java/com/example/admnistrator/bookcrossingapp/MeFragment.java
@@ -1,15 +1,17 @@
 package com.example.admnistrator.bookcrossingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.yzq.zxinglibrary.android.CaptureActivity;
+import com.yzq.zxinglibrary.bean.ZxingConfig;
+import com.yzq.zxinglibrary.common.Constant;
 
 /**
  * Created by yvemuki on 2018/3/13.
@@ -19,8 +21,16 @@ public class MeFragment extends Fragment {
     public static final String ARGUMENT = "MeFragment";
     private String mArgument;
     private View view;
+    private Button btnPosing;
+    private Button btnSwaping;
+    private Button btnReviews;
+    private Button btnRecords;
+    private Button btnSetting;
+    private Button btnWanting;
     //private FriendAdapter adapter;
     //private List<Friend> friendList = new ArrayList<>();
+    private int REQUEST_CODE_SCAN = 111; //扫码
+    private int RESULT_OK = 1;//扫码
 
     public MeFragment(){
         super();
@@ -32,7 +42,36 @@ public class MeFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_me, container, false);
         //initMeRecyclerView();
+        initButton();
         return view;
+    }
+
+    private void initButton() {
+        btnPosing = view.findViewById(R.id.me_button_posing);
+        btnSwaping = view.findViewById(R.id.me_button_swaping);
+        btnReviews =  view.findViewById(R.id.me_button_reviews);
+        btnRecords = view.findViewById(R.id.me_button_records);
+        btnSetting = view.findViewById(R.id.me_button_settings);
+        btnWanting = view.findViewById(R.id.me_button_wanting);
+        btnPosing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*ZxingConfig是配置类  可以设置是否显示底部布局，闪光灯，相册，是否播放提示音  震动等动能
+                * 也可以不传这个参数
+                * 不传的话  默认都为默认不震动  其他都为true
+                * */
+                ZxingConfig config = new ZxingConfig();
+                config.setShowbottomLayout(true);//底部布局（包括闪光灯和相册）
+                config.setPlayBeep(true);//是否播放提示音
+                config.setShake(true);//是否震动
+                config.setShowAlbum(true);//是否显示相册
+                config.setShowFlashLight(true);//是否显示闪光灯
+                //如果不传 ZxingConfig的话，两行代码就能搞定了
+                Intent intent = new Intent(getActivity(), CaptureActivity.class);
+                intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
+                startActivityForResult(intent, REQUEST_CODE_SCAN);
+            }
+        });
     }
 
     public static MeFragment newInstance(String from) {
@@ -41,6 +80,18 @@ public class MeFragment extends Fragment {
         MeFragment meFragment = new MeFragment();
         meFragment.setArguments(bundle);
         return meFragment;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 扫描二维码/条码回传
+        if (requestCode == REQUEST_CODE_SCAN && resultCode == RESULT_OK) {
+            if (data != null) {
+                String content = data.getStringExtra(Constant.CODED_CONTENT);
+                Toast.makeText(getActivity(), "扫描结果为：" + content, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 //    private void initMe(){
