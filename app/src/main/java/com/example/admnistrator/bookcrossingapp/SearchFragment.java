b31e823a0074e39e48121c1f5ec86f4f9b508200
lin2270707 <541510068@qq.com>
2018-04-04 15:43:49 +0800

    搜索内容值传递及回传
    逻辑好像不是很完善，多层传不回来

--- app/src/main/java/com/example/admnistrator/bookcrossingapp/SearchFragment.java
+++ app/src/main/java/com/example/admnistrator/bookcrossingapp/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.example.admnistrator.bookcrossingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,9 +8,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 public class SearchFragment extends Fragment {
     public static final String ARGUMENT = "SearchFragment";
@@ -17,11 +23,11 @@ public class SearchFragment extends Fragment {
     private View view;
     private SearchAdapter adapter;
     private List<Search> searchList = new ArrayList<>();
+    private View viewSearchTitle;
+    private ImageButton btnSearch;
+    private EditText editContent;
+    private String content;
 
-    public SearchFragment() {
-        super();
-        initSearch();
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,6 +35,7 @@ public class SearchFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_search, container, false);
         initSearchRecyclerView();
+        initSearch();
         return view;
     }
 
@@ -41,6 +48,24 @@ public class SearchFragment extends Fragment {
     }
 
     private void initSearch() {
+        viewSearchTitle = view.findViewById(R.id.search_title);
+        btnSearch = viewSearchTitle.findViewById(R.id.btnSearch);
+        editContent = viewSearchTitle.findViewById(R.id.editContent);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                content = editContent.getText().toString();
+                if (content.equals("") ) {
+                    Toast.makeText(getActivity(), "搜索内容不能为空白", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent intent = new Intent(getActivity(), SearchDetailActivity.class);
+                intent.putExtra("searchContent", content);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+
         for (int i = 0; i < 3; i++) {
             Search search1 = new Search(android.R.drawable.ic_menu_search, "搜索内容");
             searchList.add(search1);
@@ -54,5 +79,19 @@ public class SearchFragment extends Fragment {
         recyclerView.setLayoutManager(LayoutManager);
         adapter = new SearchAdapter(searchList);
         recyclerView.setAdapter(adapter);
+    }
+
+    //回传的值
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnContent = data.getStringExtra("searchContent");
+                    editContent.setText(returnContent);
+                }
+                break;
+            default:
+        }
     }
 }
