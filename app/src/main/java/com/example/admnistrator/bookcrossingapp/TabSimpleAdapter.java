e46ab6af1331ea9923742faf85c1a0e3ece0a67e
lin2270707 <541510068@qq.com>
2018-04-07 18:34:00 +0800

    书评的tab切换

---
+++ app/src/main/java/com/example/admnistrator/bookcrossingapp/TabSimpleAdapter.java
@@ -0,0 +1,37 @@
+package com.example.admnistrator.bookcrossingapp;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2018/4/7.
+ */
+public class TabSimpleAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragments;
+    private ArrayList<String> list;
+
+    public TabSimpleAdapter(FragmentManager fm, ArrayList<Fragment> fragments, ArrayList<String> list) {
+        super(fm);
+        this.fragments = fragments;
+        this.list = list;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return list.get(position);
+    }
+}
