06af3e89a4a3ceb000eed730025ee9b38c43532f
lin2270707 <541510068@qq.com>
2018-04-07 19:14:06 +0800

    sharelist和wantlist的数据填充
    之后需要后台支持

--- app/src/main/java/com/example/admnistrator/bookcrossingapp/ShareListActivity.java
+++ app/src/main/java/com/example/admnistrator/bookcrossingapp/ShareListActivity.java
@@ -3,13 +3,20 @@ package com.example.admnistrator.bookcrossingapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShareListActivity extends AppCompatActivity {
 
     private TextView tv_title;
     private TextView tv_username;
     private String username;
+
+    private List<BookDetail> sharelist = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,19 @@ public class ShareListActivity extends AppCompatActivity {
         username = intent.getStringExtra("username");
         tv_username.setText(username);
 
+        initData();
+        RecyclerView recyclerView = findViewById(R.id.booklist_recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        BookDetailAdapter adapter = new BookDetailAdapter(sharelist);
+        recyclerView.setAdapter(adapter);
 
+    }
+
+    private void initData() {
+        for (int i = 0; i < 3; i++) {
+            BookDetail a = new BookDetail("Yvettemuki", "《The Great Gatsby》", "下拉刷新页面", "", "");
+            sharelist.add(a);
+        }
     }
 }
