b2ebc9433cb683a50e75226ac8350388256e3f18
yvettemuki <373628977@qq.com>
2018-04-11 23:11:28 +0800

    聊天界面增加交换环节1.0实现

--- app/src/main/java/com/example/administrator/bookcrossingapp/activity/FriendChatActivity.java
+++ app/src/main/java/com/example/administrator/bookcrossingapp/activity/FriendChatActivity.java
@@ -1,5 +1,7 @@
 package com.example.administrator.bookcrossingapp.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,6 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.administrator.bookcrossingapp.datamodel.Msg;
 import com.example.administrator.bookcrossingapp.adapter.MsgAdapter;
@@ -22,18 +26,30 @@ import java.util.List;
 public class FriendChatActivity extends AppCompatActivity {
     private List<Msg> msgList = new ArrayList<>();
     private EditText inputText;
+    private Button exchangeBtn;
     private Button send;
+    private Button addBtn;
     private RecyclerView msgRecyclerView;
     private MsgAdapter adapter;
+    //点击事件时候需要变化布局
+    private LinearLayout layout_exchange;
+    private LinearLayout layout_exchange_change;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_chat);
         initMsgs(); //初始化消息数据
+
+        //根据id获取控件布局实例
         inputText = (EditText) findViewById(R.id.input_text);
         send = (Button) findViewById(R.id.send);
         msgRecyclerView = (RecyclerView) findViewById(R.id.msg_recycler_view);
+        exchangeBtn = (Button)findViewById(R.id.button_exchange);
+        addBtn = (Button)findViewById(R.id.exchange_book_right);
+        layout_exchange = (LinearLayout)findViewById(R.id.layout_chat_exchange);
+        layout_exchange_change = (LinearLayout)findViewById(R.id.layout_chat_exchange_change);
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         msgRecyclerView.setLayoutManager(layoutManager);
         adapter = new MsgAdapter(msgList);
@@ -54,6 +70,27 @@ public class FriendChatActivity extends AppCompatActivity {
                     inputText.setText(""); //清空输入框内容
 
                 }
+            }
+        });
+
+        //为点击exchange按钮设置点击事件
+        exchangeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout.LayoutParams linearParams = (LinearLayout.LayoutParams) layout_exchange.getLayoutParams();
+                linearParams.height = 400;
+                layout_exchange_change.setVisibility(View.VISIBLE);
+                Toast.makeText(getApplicationContext(), "exchange your book",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        //为添加按钮添加点击事件
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FriendChatActivity.this, ShareListActivity.class);
+                startActivity(intent);
             }
         });
     }
