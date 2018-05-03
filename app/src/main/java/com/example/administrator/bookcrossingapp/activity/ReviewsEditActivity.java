5849223532c006026b7009c11737c9b10f3c1cb7
yvettemuki <373628977@qq.com>
2018-05-03 23:49:47 +0800

    上传评论test1.0

--- app/src/main/java/com/example/administrator/bookcrossingapp/activity/ReviewsEditActivity.java
+++ app/src/main/java/com/example/administrator/bookcrossingapp/activity/ReviewsEditActivity.java
@@ -3,9 +3,11 @@ package com.example.administrator.bookcrossingapp.activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,7 +21,20 @@ import com.lzy.imagepicker.bean.ImageItem;
 import com.lzy.imagepicker.ui.ImageGridActivity;
 import com.lzy.imagepicker.view.CropImageView;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ReviewsEditActivity extends AppCompatActivity {
 
@@ -29,6 +44,7 @@ public class ReviewsEditActivity extends AppCompatActivity {
     private Button btn_write;
     private String titleValue, contentValue;
     private Drawable.ConstantState coverValue;
+    private static final String TAG = "ReviewEditActivity";
 
     private int IMAGE_PICKER = 10010;
 
@@ -89,12 +105,77 @@ public class ReviewsEditActivity extends AppCompatActivity {
                     Toast.makeText(ReviewsEditActivity.this, "请上传封面", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                //上传
+                sendPose();
                 Toast.makeText(ReviewsEditActivity.this, "上传成功，后台尚未实现", Toast.LENGTH_SHORT).show();
+
             }
 
         });
 
 
+    }
+
+    public void sendPose() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    File file = new File(getFilesDir(), "temp.jpg");//将要保存图片的路径
+                    try {
+                        Bitmap bm = ((BitmapDrawable) img_cover.getDrawable()).getBitmap();
+                        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+                        bm.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+                        bos.flush();
+                        bos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        Bitmap bm = getimage(file.getAbsolutePath());
+                        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+                        bm.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+                        bos.flush();
+                        bos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    OkHttpClient client = new OkHttpClient();
+                    RequestBody fileBody = RequestBody.create(MediaType.parse("image/png"), file);
+                    RequestBody requestBody = new MultipartBody.Builder().setType(MultipartBody.FORM).addFormDataPart("file", "img_cover.jpg", fileBody)
+                            .addFormDataPart("titleValue", titleValue + "").addFormDataPart("contentValue", contentValue)
+                            .build();
+                    Request request = new Request.Builder().url("http://120.24.217.191/Book/APP/sendReview").post(requestBody).build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    if (responseData.equals("true")) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(ReviewsEditActivity.this, "发表成功", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        });
+                    } else
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(ReviewsEditActivity.this, "服务器开小差啦", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(ReviewsEditActivity.this, "服务器开小差啦", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        }).start();
     }
 
     @Override
@@ -110,5 +191,74 @@ public class ReviewsEditActivity extends AppCompatActivity {
                 Toast.makeText(this, "没有数据", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    /**
+     * 图片按比例大小压缩方法
+     *
+     * @param srcPath （根据路径获取图片并压缩）
+     * @return
+     */
+    public static Bitmap getimage(String srcPath) {
+
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        // 开始读入图片，此时把options.inJustDecodeBounds 设回true了
+        newOpts.inJustDecodeBounds = true;
+        Bitmap bitmap = BitmapFactory.decodeFile(srcPath, newOpts);// 此时返回bm为空
+        newOpts.inSampleSize = calculateInSampleSize(newOpts, 540, 720);
+        newOpts.inJustDecodeBounds = false;
+        // 重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
+        bitmap = BitmapFactory.decodeFile(srcPath, newOpts);
+        return compressImage(bitmap);// 压缩好比例大小后再进行质量压缩
+    }
+
+    /**
+     * 质量压缩方法
+     *
+     * @param image
+     * @return
+     */
+    public static Bitmap compressImage(Bitmap image) {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);// 质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+        int options = 90;
+
+        while (baos.toByteArray().length / 1024 > 50) { // 循环判断如果压缩后图片是否大于100kb,大于继续压缩
+            Log.i(TAG, "compressImage: " + baos.toByteArray().length);
+            baos.reset(); // 重置baos即清空baos
+            image.compress(Bitmap.CompressFormat.JPEG, options, baos);// 这里压缩options%，把压缩后的数据存放到baos中
+            options -= 10;// 每次都减少10
+        }
+        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());// 把压缩后的数据baos存放到ByteArrayInputStream中
+        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);// 把ByteArrayInputStream数据生成图片
+        Log.i(TAG, "压缩后图片的大小" + (image.getByteCount() / 1024)
+                + "kb宽度为" + image.getWidth() + "高度为" + image.getHeight());
+        return bitmap;
+    }
+
+    private static int calculateInSampleSize(BitmapFactory.Options options,
+                                             int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and
+            // width
+            final int heightRatio = Math.round((float) height
+                    / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will
+            // guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? widthRatio : heightRatio;
+        }
+
+        return inSampleSize;
     }
 }
