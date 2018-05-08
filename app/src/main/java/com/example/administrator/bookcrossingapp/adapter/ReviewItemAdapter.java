f9477e2dcae2da820f9e653b468b9041887c4ec9
OUC-MYM <570109323@qq.com>
2018-05-08 21:54:19 +0800

    修复bug

--- app/src/main/java/com/example/administrator/bookcrossingapp/adapter/ReviewItemAdapter.java
+++ app/src/main/java/com/example/administrator/bookcrossingapp/adapter/ReviewItemAdapter.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,16 +17,14 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.administrator.bookcrossingapp.R;
-import com.example.administrator.bookcrossingapp.datamodel.BookDetail;
-import com.example.administrator.bookcrossingapp.datamodel.ReviewItem;
 import com.example.administrator.bookcrossingapp.activity.ReviewsDetailActivity;
+import com.example.administrator.bookcrossingapp.datamodel.ReviewItem;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
@@ -39,7 +36,7 @@ import okhttp3.Response;
  * Created by Administrator on 2018/4/8.
  */
 
-public class ReviewItemAdapter  extends RecyclerView.Adapter<ReviewItemAdapter.ViewHolder>{
+public class ReviewItemAdapter extends RecyclerView.Adapter<ReviewItemAdapter.ViewHolder> {
     private List<ReviewItem> mReviewList;
     private static final String TAG = "ReviewItemAdapter";
     private Context context;
@@ -68,7 +65,7 @@ public class ReviewItemAdapter  extends RecyclerView.Adapter<ReviewItemAdapter.V
         mReviewList = reviewItemList;
     }
 
-    public ReviewItemAdapter(List<ReviewItem> reviewItemList, Context context, Activity mActivity){
+    public ReviewItemAdapter(List<ReviewItem> reviewItemList, Context context, Activity mActivity) {
         this.context = context;
         this.mActivity = mActivity;
         mReviewList = reviewItemList;
@@ -86,17 +83,17 @@ public class ReviewItemAdapter  extends RecyclerView.Adapter<ReviewItemAdapter.V
                 //item点击事件
                 int position = holder.getAdapterPosition();
                 ReviewItem reviewItem = mReviewList.get(position);
-                Intent intent = new Intent(v.getContext(),ReviewsDetailActivity.class);
+                Intent intent = new Intent(v.getContext(), ReviewsDetailActivity.class);
                 intent.putExtra("articleId", reviewItem.getArticleId());
-                intent.putExtra("cover",reviewItem.getCoverImgUrl());
-                intent.putExtra("title",reviewItem.getTitle());
-                intent.putExtra("author",reviewItem.getAuthor());
-                intent.putExtra("likeAmount",reviewItem.getLikeAmount());
+                intent.putExtra("cover", reviewItem.getCoverImgUrl());
+                intent.putExtra("title", reviewItem.getTitle());
+                intent.putExtra("author", reviewItem.getAuthor());
+                intent.putExtra("likeAmount", reviewItem.getLikeAmount());
                 view.getContext().startActivity(intent);
             }
         });
 
-        holder.btn_like.setOnClickListener(new View.OnClickListener(){
+        holder.btn_like.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //点赞点击事件
@@ -112,23 +109,23 @@ public class ReviewItemAdapter  extends RecyclerView.Adapter<ReviewItemAdapter.V
                             OkHttpClient client = new OkHttpClient();
                             //获取articleId和userId
                             int articleId = reviewItem.getArticleId();
-                            SharedPreferences pref = context.getSharedPreferences("user_info",Context.MODE_PRIVATE);
-                            int userId = pref.getInt("userid",0);
+                            SharedPreferences pref = context.getSharedPreferences("user_info", Context.MODE_PRIVATE);
+                            int userId = pref.getInt("userid", 0);
 
                             RequestBody requestBody = new FormBody.Builder()
-                                    .add("articleId", articleId+"")
-                                    .add("userId", userId+"")
+                                    .add("articleId", articleId + "")
+                                    .add("userId", userId + "")
                                     .build();
                             Request request = new Request.Builder().url("http://120.24.217.191/Book/APP/likeQuery").post(requestBody).build();
                             Response response = client.newCall(request).execute();
-                            Log.i("test_like_query111111",response.toString());
+                            Log.i("test_like_query111111", response.toString());
                             //返回数据
 
-                            if(response.isSuccessful()){
+                            if (response.isSuccessful()) {
                                 //获取点赞成功与否标志
                                 String responseData = response.body().string();
                                 boolean sign = handleResponseData(responseData);
-                                if(sign){
+                                if (sign) {
                                     mActivity.runOnUiThread(new Runnable() {
                                         @Override
                                         public void run() {
@@ -136,11 +133,10 @@ public class ReviewItemAdapter  extends RecyclerView.Adapter<ReviewItemAdapter.V
                                             Drawable drawable = context.getResources().getDrawable(R.drawable.btn_review_like_tap);
                                             view.setBackground(drawable);
                                             int newAmount = reviewItem.getLikeAmount() + 1;
-                                            holder.tv_like.setText(newAmount+"");
+                                            holder.tv_like.setText(newAmount + "");
                                         }
                                     });
-                                }
-                                else {
+                                } else {
                                     mActivity.runOnUiThread(new Runnable() {
                                         @Override
                                         public void run() {
@@ -151,13 +147,13 @@ public class ReviewItemAdapter  extends RecyclerView.Adapter<ReviewItemAdapter.V
 
                             }
                         } catch (IOException e) {
-                                e.printStackTrace();
-                                mActivity.runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Toast.makeText(mActivity, "服务器开小差啦", Toast.LENGTH_SHORT).show();
-                                    }
-                                });
+                            e.printStackTrace();
+                            mActivity.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(mActivity, "服务器开小差啦", Toast.LENGTH_SHORT).show();
+                                }
+                            });
                         }
 
                     }
@@ -171,9 +167,9 @@ public class ReviewItemAdapter  extends RecyclerView.Adapter<ReviewItemAdapter.V
     @Override
     public void onBindViewHolder(ReviewItemAdapter.ViewHolder holder, int position) {
         ReviewItem reviewItem = mReviewList.get(position);
-        holder.tv_author.setText(reviewItem.getAuthor());
+        holder.tv_author.setText("by " + reviewItem.getAuthor());
         holder.tv_title.setText(reviewItem.getTitle());
-        holder.tv_like.setText(reviewItem.getLikeAmount()+"");
+        holder.tv_like.setText(reviewItem.getLikeAmount() + "");
         Glide.with(context).load("http://120.24.217.191/Book/img/reviewImg/" + reviewItem.getCoverImgUrl()).into(holder.img_cover);
     }
 
@@ -186,11 +182,10 @@ public class ReviewItemAdapter  extends RecyclerView.Adapter<ReviewItemAdapter.V
     public boolean handleResponseData(final String responseData) {
         try {
             JSONArray jsonArray = new JSONArray(responseData);
-            if(jsonArray.length() > 0){
+            if (jsonArray.length() > 0) {
                 JSONObject jsonObject = jsonArray.getJSONObject(0);
                 return jsonObject.getBoolean("sign");
-            }
-            else return false;
+            } else return false;
 
         } catch (Exception e) {
             e.printStackTrace();
